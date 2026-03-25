9ecdb09585d94e6b0557cc580b8fe6eca93cd4ab
AidaBc87 <aida.bolivar.tap@gmail.com>
2026-03-25 16:37:15 -0700

    Create mapas.dart

---
+++ mapas.dart
@@ -0,0 +1,15 @@
+void main() {
+for (int i = 1; i <= 5; i++) {
+print(i);
+}
+  print("segundo ejemplo for");
+List<String> arcoiris=['verde','azul', 'Amarillo','Violeta'];
+  
+for (String color in arcoiris) {
+print(color);
+}
+  
+print("Otra salida");
+  print(arcoiris);
+  
+}
